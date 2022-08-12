afc83f8d55cb7c61247b6bd8daddb770247fa12b
Mash <104835532+Mash-7@users.noreply.github.com>
2022-08-13 00:33:43 +0200

    header git

--- shell.h
+++ shell.h
@@ -1,7 +1,9 @@
 #ifdef _SHELL_H_
 #define _SHELL_H_
 
-extern char **environ;
+extern char **environ; /* global environment */
+char *name; /* program name */
+int hist; /* history counter */
 
 /**###### LIBRARIES ######*/
 #include <stdio.h>
@@ -20,4 +22,67 @@ extern char **environ;
 #define DELIM " \t\r\n\a"
 #define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
 
+/**###### FUNCTIONS ######*/
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+size_t _strncmp(char *s1, char *s2, size_t n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
 
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
+
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+
+void prompt(void);
+void signal_to_handel(int sig);
+char *_getline(void);
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+void help_all(void);
+void help_alias(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+/**
+struct buitin - Defines builtin commands.
+@command: The command.
+@f: Function pointer to the builtin command function.
+/
+typedef struct  builtin
+{
+	char *command;
+	int (*f)(char **line, int er);
+} builtin_t;
+
+#endif
